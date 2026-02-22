1df4f574caf642ce04b78d65e032af048b82f803
Kamalovic Dev <kamalovicdev@gmail.com>
2026-02-22 15:35:56 +0500

    Update footer text and structure in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ function App() {
       {/* Footer qismi */}
       <footer className="py-6 text-center border-t border-slate-900 bg-slate-950">
         <p className="text-slate-500 text-sm font-mono">
-          &copy; {currentYear} KAMALOVIC. All rights reserved. <br/>
-          <span className="text-xs text-slate-600">Built with React & Tailwind CSS</span>
+          &copy; {currentYear} KAMALOVIC  <br/>
+          <span className="text-xs text-slate-600">All rights reserved.</span>
         </p>
       </footer>
     </div>
